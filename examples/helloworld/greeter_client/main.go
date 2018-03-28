6cfb467f41bfa99277c87c317e505e75228f4065
Santosh Ananthakrishnan <santosh.ankr@gmail.com>
2018-03-27 22:44:28 -0700

    Make greeter client and server use TLS with session resumption

--- examples/helloworld/greeter_client/main.go
+++ examples/helloworld/greeter_client/main.go
@@ -19,12 +19,17 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
@@ -33,9 +38,54 @@ const (
 	defaultName = "world"
 )
 
+func buildConfig() *tls.Config {
+	rootPem, err := ioutil.ReadFile("../certs/CAcert.pem")
+	if err != nil {
+		fmt.Printf("Error reading root CA file: %s\n", err)
+		return nil
+	}
+	rootCerts := x509.NewCertPool()
+	ok := rootCerts.AppendCertsFromPEM(rootPem)
+	if !ok {
+		fmt.Printf("Error append root CA cert to pool: %s\n", err)
+		return nil
+	}
+
+	cert, err := tls.LoadX509KeyPair("../certs/client.crt", "../certs/client.pem")
+	if err != nil {
+		fmt.Printf("Error loading client certificates: %s\n", err)
+		return nil
+	}
+
+	ticketCache := tls.NewLRUClientSessionCache(32)
+	c := tls.Config{
+		Certificates:       []tls.Certificate{cert},
+		ServerName:         "server",
+		ClientSessionCache: ticketCache,
+		RootCAs:            rootCerts,
+	}
+
+	return &c
+}
+
 func main() {
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	config := buildConfig()
+
+	// Client 1
+	connect(config, name)
+
+	// Client 2
+	connect(config, name)
+}
+
+func connect(config *tls.Config, name string) {
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	creds := credentials.NewTLS(config)
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -43,10 +93,6 @@ func main() {
 	c := pb.NewGreeterClient(conn)
 
 	// Contact the server and print out its response.
-	name := defaultName
-	if len(os.Args) > 1 {
-		name = os.Args[1]
-	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
--- examples/helloworld/greeter_server/main.go
+++ examples/helloworld/greeter_server/main.go
@@ -21,12 +21,19 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"strconv"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/reflection"
 )
 
@@ -39,7 +46,44 @@ type server struct{}
 
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+	peer, ok := peer.FromContext(ctx)
+	if !ok {
+		fmt.Println("Could not get peer from context")
+	}
+	tlsInfo := (peer.AuthInfo).(credentials.TLSInfo)
+	didResume := tlsInfo.State.DidResume
+	return &pb.HelloReply{Message: in.Name + " resumed connection? " + strconv.FormatBool(didResume)}, nil
+}
+
+func buildConfig() *tls.Config {
+	rootPem, err := ioutil.ReadFile("../certs/CAcert.pem")
+	if err != nil {
+		fmt.Printf("Error reading root CA file: %s\n", err)
+		return nil
+	}
+	rootCerts := x509.NewCertPool()
+	ok := rootCerts.AppendCertsFromPEM(rootPem)
+	if !ok {
+		fmt.Printf("Error append root CA cert to pool: %s\n", err)
+		return nil
+	}
+
+	cert, err := tls.LoadX509KeyPair("../certs/server.crt", "../certs/server.pem")
+	if err != nil {
+		fmt.Printf("Error loading server certificates: %s\n", err)
+		return nil
+	}
+
+	var sessionTicketKey [32]byte
+	copy(sessionTicketKey[:], "abcdefghijklmnopqrstuvwxyz012345")
+	c := tls.Config{
+		Certificates:     []tls.Certificate{cert},
+		ClientCAs:        rootCerts,
+		ClientAuth:       tls.VerifyClientCertIfGiven,
+		SessionTicketKey: sessionTicketKey,
+	}
+
+	return &c
 }
 
 func main() {
@@ -47,7 +91,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+
+	config := buildConfig()
+	creds := credentials.NewTLS(config)
+
+	s := grpc.NewServer(grpc.Creds(creds))
 	pb.RegisterGreeterServer(s, &server{})
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
